e7d2d9979d99dfa0bcc05ec26b2cd02aacf8bbb6
Bakir Talibov <bakirt@wandafi.com>
2022-04-27 20:57:29 +0400

    added client socket connection and logging

--- src/main/java/DateServer.java
+++ src/main/java/DateServer.java
@@ -9,15 +9,19 @@ public class DateServer {
     public static final int PORT = 9090;
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("(SERVER) Waiting for client connection...");
         Socket client = serverSocket.accept();
+        System.out.println("(SERVER) Connected to client!");
         PrintWriter out = new PrintWriter(client.getOutputStream(),true);
 
         System.out.println("SocketServer info:" + serverSocket.toString());
         System.out.println("Socket info:" + client.toString());
 
         //Print current time
-        out.println("Current Time is:" + LocalDateTime.now());
+        String currentDateTime = LocalDateTime.now().toString();
+        out.println("Current Time is:" + currentDateTime);
         client.close();
+        System.out.println("(SERVER) Sent date (" + currentDateTime + ") and closed the socket.");
         serverSocket.close();
         //reopen
         main(new String[0]);
